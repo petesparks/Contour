eb91cb68a0dbf3d8a839fcd7ac9e51262f0a5448
Peter Sparks <petersparks18@gmail.com>
2020-02-15 19:17:01 +0000

    removed js for video

--- app/javascript/components/video.js
+++ app/javascript/components/video.js
@@ -1,17 +1,17 @@
-var videoRemove = document.getElementById('myVideo');
-var videoAdd = document.getElementById('myVideo');
+// var videoRemove = document.getElementById('myVideo');
+// var videoAdd = document.getElementById('myVideo');
 
 // check first if the page is on services page via params
 
-if (window.location.pathname == ('/services')) {
-  window.onresize = window.onload = function () {
-    if (window.innerWidth < 350) {
+// if (window.location.pathname == ('/services')) {
+//   window.onresize = window.onload = function () {
+//     if (window.innerWidth < 350) {
 
-        videoRemove.setAttribute('src','');
-    } else {
+//         videoRemove.setAttribute('src','');
+//     } else {
 
-      videoAdd.setAttribute('src', 'https://res.cloudinary.com/lwsw18ldn/video/upload/q_auto:low/v1547115237/Contour/ContourMovie_1.mp4')
-    }
+//       videoAdd.setAttribute('src', 'https://res.cloudinary.com/lwsw18ldn/video/upload/q_auto:low/v1547115237/Contour/ContourMovie_1.mp4')
+//     }
 
-  }
-};
+//   }
+// };
